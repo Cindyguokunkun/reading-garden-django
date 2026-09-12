f27f14ddd6085fb7c98ac2899e446f0f531f431b
郭坤坤 <local@reading-garden>
2026-09-12 10:09:00 +0800

    Test quiz answer review

--- reading/tests.py
+++ reading/tests.py
@@ -27,6 +27,16 @@ class ReadingGardenTests(TestCase):
         self.assertContains(response,'100%')
         self.assertEqual(ReadingRecord.objects.count(),1)
 
+    def test_wrong_answers_can_be_reviewed(self):
+        response=self.client.post(reverse('quiz_start'),{'class':self.room.pk,'student':self.student.pk,'book':self.book.pk})
+        attempt_id=int(response.url.strip('/').split('/')[-1])
+        questions=self.client.session[f'quiz_{attempt_id}']
+        answers={f'q{i}':str((q['answer']+1)%4) for i,q in enumerate(questions)}
+        response=self.client.post(reverse('quiz_take',args=[attempt_id]),answers)
+        self.assertContains(response,'Review Answers')
+        self.assertContains(response,'Correct answer:')
+        self.assertEqual(ReadingRecord.objects.count(),0)
+
     def test_excel_export(self):
         response=self.client.get(reverse('export_excel')+f'?class={self.room.pk}')
         self.assertEqual(response.status_code,200)
